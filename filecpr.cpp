6c66926131e626cd1c8b6bf0ed4e97b824b6b557
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-14 17:52:35 +0530

    Step 1

--- filecpr.cpp
+++ filecpr.cpp
@@ -23,3 +23,39 @@ long getFileSize(const string &filename) {
 int main() {
     string fileA = "Original_Record.csv";
     string fileB = "Temporary_Record.csv";
+
+// STEP 1: Check if Original_Record exists
+
+    if (!fileExists(fileA)) {
+        ofstream outA(fileA);
+        int n;
+        cout << "File 'Original_Record' not found.Please Wait Creating it...\n";
+        cout << "Enter number of records: ";
+        cin >> n;
+        cin.ignore();
+
+        // Write header
+        outA << "Name,Age,Class,Div,Teacher\n";
+
+        // Write data
+        for (int i = 0; i < n; i++) {
+            string name, cls, div, teacher;
+            int age;
+            cout << "\nRecord " << i + 1 << ":\n";
+            cout << "Name: ";
+            getline(cin, name);
+            cout << "Age: ";
+            cin >> age;
+            cin.ignore();
+            cout << "Class: ";
+            getline(cin, cls);
+            cout << "Division: ";
+            getline(cin, div);
+            cout << "Teacher: ";
+            getline(cin, teacher);
+
+            outA << "\"" << name << "\"," << age << ",\"" << cls << "\",\"" << div << "\",\"" << teacher << "\"\n";
+        }
+        outA.close();
+        cout << "Original_Record created successfully.\n";
+    }
