0220252647853a62a7a89bd46ef5e58d015f252b
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-08-14 17:37:49 +0530

    created function  Check if file has data

--- filecpr.cpp
+++ filecpr.cpp
@@ -9,3 +9,9 @@ bool fileExists(const string &filename) {
     struct stat buffer;
     return (stat(filename.c_str(), &buffer) == 0);
 }
+
+// Check if file has data
+bool fileHasData(const string &filename) {
+    ifstream file(filename);
+    return file.peek() != ifstream::traits_type::eof();
+}
